ffb0b8feddf5420f68fb1f99e5c27381f2725075
Martin <martin.rojo@colegioalfinach.com>
2021-05-13 10:35:34 +0200

    funcion  Negamax completa + desordenar casillas

--- tictactoe_data.py
+++ tictactoe_data.py
@@ -46,7 +46,12 @@ def end_game():
         
     return  out    
 
-
+def desordenar_casillas(l2):
+    l1=[]
+    l1.extend(l2)
+    random.shuffle(l1)
+    return l1
+    
 #################################################################################################
 ## Funcion recursiva: (funcion que se llama a si misma) ########################################
 
@@ -62,4 +67,39 @@ def Negamax(board,depth,alpha=-inf, beta=+inf):
             return (score,None) 
         else: 
             return  ((score - 0.01*depth*abs(score)/score),None) # valoro la progundida para busjar victorias
-                                                                 # rapidas o derrotas lentas. 
+                                                                 # rapidas o derrotas lentas.
+
+    # caso general: 
+    # Probamos jugadas como jugador y como oponente hasta que encontremos la jugada optima 
+
+    # En la primer jugada,elegimos la casilla de forma aleatoria para que el juego sea mas variado. 
+    # Si no el ordenador empezaria siempre de la misma forma.
+
+    if len(possible_moves())==9:
+        return (None,random.randint(1,9))
+
+
+    bestValue = -inf
+
+    for move in desordenar_casillas(possible_moves()):  # para todas las casillas disponibles    
+        make_move(move)                                 # juego en esa casilla (jugada de prueba)
+        switch_player()                                 # cambio de jugdor a oponente
+        value = - (Negamax(board, depth+1,-beta, -alpha)[0])  # juego como el oponente
+        switch_player()                                       # cambio de oponente a jugador
+        undo_move(move)                                       # deshago la jugada de pueba
+
+        if value > bestValue:     # si es la mejor jugada
+            bestValue=value       # guardo el valor de la jugada
+            bestMove=move         # guardo la casilla de la jugada.
+        
+        # alpha = limite inferior forzado por el oponente al jugador (inicialmente -inf)
+        # beta  =  limite superior forzado por el jugador al oponente (inicialmente inf)
+        
+        if  alpha < value :      
+                alpha = value      # actualizo el lim inf consegudo por el oponente
+
+
+        if (alpha >= beta):        # ignoro los casos o jugadas en que los lim inf y sup son >=
+            break                  # ya que son casos redundantes
+
+    return (bestValue,bestMove)    # devuelvo el valor y la casilla de la juga
